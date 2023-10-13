6a871d63ef56b4c2c8d7ba48853e4f62612be11c
vincentkho67 <vincentkho67@gmail.com>
2023-10-14 01:14:18 +0700

    finish multer

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express()
 const port = 3000
 const router = require("./routes/index")
 const errorHandler = require('./middleware/errorHandler')
+const uploadRouter = require("./upload.js")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.use(uploadRouter)
 app.use(router)
 app.use(errorHandler)
 
